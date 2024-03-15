10702b338bd6247d6899890d24c940e73c9f90d1
cyrus925 <cyrus.larger@epfedu.fr>
2024-03-15 11:52:03 +0000

    tp2 not finish

--- app.js
+++ app.js
@@ -1,11 +1,44 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const path = require("path");
+app.set('views', './views');
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.sendFile(path.join(__dirname,"./views/home.html"))
 })
+
+
 app.get('/students', (req, res) => {
+  
+    const rowSeparator = '\r\n';
+    const cellSeparator = ';';
+    fs.readFile('data.csv', 'utf8', (err, data) => {
+  
+      const rows = data.split(rowSeparator);
+      console.log("rows",rows);
+      const [headerRow, ...contentRows] = rows
+      const header = headerRow.split(cellSeparator);
+      console.log("header",header);
+      const students = contentRows.map((row) => {
+        const cells = row.split(cellSeparator);
+        const student = {
+          [header[0]]: cells[0],
+          [header[1]]: cells[1],
+          // Ajoutez ici les autres colonnes du fichier CSV si nécessaire
+        };
+        console.log("student name",student.name);
+        return student;
+      });
+  
+      console.log('students',students);
+      res.render("students", { students: students });
+    });
+  });
+
+
+app.get('/api/students', (req, res) => {
   res.send([{ name: "Eric Burel", school: "EPF" }, { name: "HarryPotter", school: "Poudlard" }])
 })
 
@@ -14,8 +47,8 @@ app.listen(port, () => {
 })
 
 const fs = require("fs")
-app.get('/students-csv', (req, res) => {
-  fs.readFile('data2.csv', 'utf8', (err, data) => {
+app.get('/api/students-csv', (req, res) => {
+  fs.readFile('data.csv', 'utf8', (err, data) => {
     if (err) {
       console.error('Une erreur s\'est produite lors de la lecture du fichier :', err);
       return;
@@ -24,33 +57,10 @@ app.get('/students-csv', (req, res) => {
   });
 })
 
-app.get('/students-csv-parsed', (req, res) => {
-  const rowSeparator = '\r\n';
-  const cellSeparator = ';';
-
-  fs.readFile('data.csv', 'utf8', (err, data) => {
-
-    const rows = data.split(rowSeparator);
-    const [headerRow, ...contentRows] = rows;
-    const header = headerRow.split(cellSeparator);
-
-    const students = contentRows.map((row) => {
-      const cells = row.split(cellSeparator);
-      const student = {
-        [header[0]]: cells[0],
-        [header[1]]: cells[1],
-        // Ajoutez ici les autres colonnes du fichier CSV si nécessaire
-      };
-      return student;
-    });
-
-    res.send(students);
-  });
-});
 
 
 app.use(express.json())
-app.post('/students/create', (req, res) => {
+app.post('/api/students/create', (req, res) => {
   console.log(req.body)
   const csvLine =`\n${req.body.name};${req.body.school}` 
   console.log('csvLine', csvLine);
@@ -59,3 +69,6 @@ app.post('/students/create', (req, res) => {
   }) 
   res.send("Student created")
 })
+
+
+
