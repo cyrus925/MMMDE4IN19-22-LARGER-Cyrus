82181045f068ccdee1751248b43a1b3f34f9555b
cyrus925 <cyrus.larger@epfedu.fr>
2024-03-20 08:48:11 +0000

    working tp1 and tp2

--- app.js
+++ app.js
@@ -6,37 +6,46 @@ app.set('views', './views');
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname,"./views/home.html"))
+  res.sendFile(path.join(__dirname, "./views/home.html"))
 })
 
 
 app.get('/students', (req, res) => {
-  
-    const rowSeparator = '\r\n';
-    const cellSeparator = ';';
-    fs.readFile('data.csv', 'utf8', (err, data) => {
-  
+  const rowSeparator = "\n";
+  const cellSeparator = ";";
+  fs.readFile(
+    "data.csv",
+    "utf8",
+    (err, data) => {
       const rows = data.split(rowSeparator);
-      console.log("rows",rows);
-      const [headerRow, ...contentRows] = rows
+      const [headerRow, ...contentRows] = rows;
       const header = headerRow.split(cellSeparator);
-      console.log("header",header);
-      const students = contentRows.map((row) => {
+      const students = contentRows.map(row => {
         const cells = row.split(cellSeparator);
         const student = {
-          [header[0]]: cells[0],
-          [header[1]]: cells[1],
-          // Ajoutez ici les autres colonnes du fichier CSV si nécessaire
+          name: cells[0],
+          school: cells[1]
         };
-        console.log("student name",student.name);
         return student;
       });
-  
-      console.log('students',students);
       res.render("students", { students: students });
     });
   });
 
+
+app.get('/students/create', (req,res)=>{
+  res.render("create-student")
+})
+
+app.use(express.urlencoded({ extended: true }));
+app.post('/students/create', (req,res)=>{
+  console.log(req.body);
+  const csvLine = `\r\n${req.body.name};${req.body.school}`
+  console.log('csvLine', csvLine);
+  const stream = fs.writeFile('data.csv', csvLine, { flag: "a" }, (err) => {
+    res.redirect("/students/create?created=1");
+  })
+})
 
 app.get('/api/students', (req, res) => {
   res.send([{ name: "Eric Burel", school: "EPF" }, { name: "HarryPotter", school: "Poudlard" }])
@@ -58,16 +67,37 @@ app.get('/api/students-csv', (req, res) => {
 })
 
 
+app.get('/api/students-csv-parsed', (req, res) => {
+  const rowSeparator = "\n";
+  const cellSeparator = ";";
+  fs.readFile(
+    "data.csv",
+    "utf8",
+    (err, data) => {
+      const rows = data.split(rowSeparator);
+      const [headerRow, ...contentRows] = rows;
+      const header = headerRow.split(cellSeparator);
+      const students = contentRows.map(row => {
+        const cells = row.split(cellSeparator);
+        const student = {
+          name: cells[0],
+          school: cells[1]
+        };
+        return student;
+      });
+      console.log(students);
+    });
+});
+
 
 app.use(express.json())
 app.post('/api/students/create', (req, res) => {
   console.log(req.body)
-  const csvLine =`\n${req.body.name};${req.body.school}` 
+  const csvLine = `\r\n${req.body.name};${req.body.school}`
   console.log('csvLine', csvLine);
-   const stream=fs.writeFile('data.csv', csvLine, { flag: "a" }, (err) => {
+  const stream = fs.writeFile('data.csv', csvLine, { flag: "a" }, (err) => {
     res.send('ok');
-  }) 
-  res.send("Student created")
+  })
 })
 
 
